96fdc4bae7da4f7be003d2af543e2d296e84fb55
Tomas Pierce <piercetf@nih.gov>
2024-11-18 11:13:27 -0500

    Add in data getting to user interface

--- cetsa_interface.py
+++ cetsa_interface.py
@@ -47,31 +47,62 @@ class FilenameOpenChooser(tk.Frame):
     def get(self):
         return self.textvariable.get()
 
-NOT_SET = 0
-COMBO_T_TEST = 1
-NPARC = 2
+
+class MethodChooser(tk.Frame):
+    
+    NOT_SET = 0
+    COMBO_T_TEST = 1
+    NPARC = 2
+    
+    def __init__(self, master, variable):
+        tk.Frame.__init__(self, master)
+        
+        self.variable = variable
+        
+        self.ita_radio = ttk.Radiobutton(self, 
+                                         text="ITA", 
+                                         variable=variable,
+                                         value=self.COMBO_T_TEST)
+        
+        self.nparc_radio = ttk.Radiobutton(self,
+                                           text="NPARC",
+                                           variable=variable,
+                                           value=self.NPARC)
+        
+        self.method_label = ttk.Label(self,
+                                      text="Analysis method")
+        
+        self.method_label.grid(row=0, column=0)
+        self.ita_radio.grid(row=1, column=0)
+        self.nparc_radio.grid(row=2, column=0)
+    
+    def get(self):
+        return self.variable.get()
 
 
 window = tk.Tk(screenName="CETSA Pipeline")
 
-approach = tk.IntVar(value=NOT_SET)
+approach = tk.IntVar(value=MethodChooser.NOT_SET)
+
+method_choose = MethodChooser(window, approach)
 
 candidate_filepath = tk.StringVar()
 
-
-methodlab = ttk.Label(window, text="Analysis method")
-itabut = ttk.Radiobutton(master=window, variable=approach, text="ITA", value=COMBO_T_TEST)
-nparcbut = ttk.Radiobutton(master=window, variable=approach, text="NPARC", value=NPARC)
+data_filepath = tk.StringVar()
 
 candidate_ask = FilenameOpenChooser(window, 
                                     "Candidate", 
                                     candidate_filepath,
                                     cetsa_paths.get_candidates_filepath(False))
 
+data_ask = FilenameOpenChooser(window,
+                               "Data",
+                               data_filepath,
+                               cetsa_paths.get_data_filepath(False))
+
 candidate_ask.pack()
-methodlab.pack()
-itabut.pack()
-nparcbut.pack()
+data_ask.pack()
+method_choose.pack()
 
 window.mainloop()
 
