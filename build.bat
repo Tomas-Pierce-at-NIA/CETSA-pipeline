9a5fef369a784849a46596c48c1678eabcc82ad5
Tomas Pierce <piercetf@nih.gov>
2025-01-30 10:13:01 -0500

    finish building steps

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 REM Building script for the SenoCETSA project
-
-conda activate senocetsa
+REM ensure an appropriate conda environment is active before running
 
 pyinstaller --name SenoCETSA^
   --add-data "README.md:."^
