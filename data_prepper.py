29a19b86193616674d95135fe0b81f81f85346ad
Tomas Pierce <piercetf@nih.gov>
2025-04-07 16:23:52 -0400

    Started implemented Bayes Factor calculation

--- data_prepper.py
+++ data_prepper.py
@@ -50,3 +50,20 @@ class DataPreparer:
         colors = seaborn.color_palette('hls', len(self.category_names))
         pal = dict(zip(self.category_names, colors))
         return pal
+    
+    
+    def null_model_cols_transform(self, focused_data, category1, category2):
+        categories = self.onehot.transform(focused_data.loc[:, ['Treatment']]).toarray()
+        focused_data.loc[:, self.category_names] = categories
+        scaled_temps = self.scaler.transform(focused_data[['Temperature']])
+        focused_data.loc[:, ['ScaledTemp']] = scaled_temps
+        focal_data = focused_data[(focused_data[category1] == 1) | (focused_data[category2] == 1)]
+        treatments = focal_data['Treatment']
+        prot_ids = focal_data[['PG.ProteinAccessions', 'PG.Genes']]
+        interact_vars = self.aware_poly.fit_transform(focal_data[['ScaledTemp',
+                                                                  category1,
+                                                                  category2]])
+        col_count = 2 # bias + temp
+        nh_data = interact_vars[:, col_count]
+        outputs = focal_data[NORMPROT].to_numpy()
+        return nh_data, outputs, treatments, prot_ids
